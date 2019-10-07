785ff801bc36283c084982fa5a5d8e00ca30eb3b
jaybenaim <benaimjacob@gmail.com>
2019-10-07 02:24:45 -0400

    adding heroku node buildpack

--- shop_it/views.py
+++ shop_it/views.py
@@ -20,7 +20,6 @@ class FrontendAppView(View):
     Serves the compiled frontend entry point (only works if you have run `yarn
     run build`).
     """
-
     def get(self, request):
         try:
             with open(os.path.join(settings.REACT_APP_DIR, 'build', 'index.html')) as f:
