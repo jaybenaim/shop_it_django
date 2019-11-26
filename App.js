6a886c4432da369c596bf22587a4f03f9173761c
jaybenaim <benaimjacob@gmail.com>
2019-11-25 21:42:41 -0500

    link to homepage fix with app.js in root

--- App.js
+++ App.js
@@ -3,32 +3,46 @@ import "./App.css";
 import Home from "./shop_it_frontend/src/components/Home";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import ShoppingListPage from "./shop_it_frontend/src/components/ShoppingListPage.js";
-
+import Store from "./shop_it_frontend/src/components/Store";
+import SearchPage from "./shop_it_frontend/src/components/SearchPage";
 function App() {
   return (
-    <div className="App container">
+    <Container fluid={true} className="App container">
       <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/shoppingList">Create a shopping list</Link>
-            </li>
-          </ul>
-        </div>
+        <Nav className="justify-content-center navbar">
+          <Nav.Item>
+            <Link to="/shop_it_django">Home</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to="shopping_list"> Shopping List</Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to="stores"> Stores </Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Link to="search"> Suggestions </Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Register />
+          </Nav.Item>
+        </Nav>
+
         <Switch>
-          <Route exact path="/">
+          <Route exact path="/shop_it_django/">
             <Home />
           </Route>
-          <Route path="/shoppingList">
+          <Route path="/shopping_list">
             <ShoppingListPage />
+          </Route>
+          {/*  if admin  */}
+          <Route exact path="/stores">
+            <Store />
+          </Route>
+          <Route exact path="/search">
+            <SearchPage />
           </Route>
         </Switch>
       </Router>
-    </div>
+    </Container>
   );
 }
-
-export default App;
